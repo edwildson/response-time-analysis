e64c47e05efcfa0f6ce4946d8d984ee6cffd6c37
Edwildson Coelho Rodrigues <edwildson@hotmail.com>
2022-06-09 06:07:44 -0300

    Cria página princial

--- response_time_analysis/urls.py
+++ response_time_analysis/urls.py
@@ -13,9 +13,12 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from urllib import response
 from django.contrib import admin
 from django.urls import path
+from response_time import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('',    views.index),
 ]
