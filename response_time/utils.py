32c82f273259c8d7d54faa6469ef0962f9807c76
Edwildson Coelho Rodrigues <edwildson@hotmail.com>
2022-06-11 12:28:49 -0300

    implements analysis with jitter = 0

--- response_time/utils.py
+++ response_time/utils.py
@@ -1,44 +1,96 @@
 from math import ceil
+from multiprocessing.spawn import import_main_path
+from urllib import response
 from tablib import Dataset
+from django.http import JsonResponse 
 
 def analysis(data):
     data_to_analysis = data
     dataset = Dataset()
     imported_data = dataset.load(data_to_analysis.read(),format='xlsx')
-    print(imported_data)
+    for (index, data) in enumerate(imported_data):
+        print(index, data)
+    
 
     response_time_dict = {}
 
-    for (index, data) in enumerate(imported_data):
-        response_time_dict[index] = []
-        counter = 0
-        if not index:
-            response_time_dict[index] = [data[3]]
-            print("Index 0")
-            continue
+    U = 0
 
-        while True :
-            print(counter)
-            if not counter:
-                response_time_dict[index] = [data[3]]            
+    for data in imported_data:
+        if None not in data:
+            U += data[3]/data[1]
+        else:
+            del imported_data[-1]
+            
 
-            else:
-                j = 0
-                sum = 0
-                while j < index: 
-                    # print("Percorrendo o conjunto")
-                    print(j)
-                    sum += ceil(response_time_dict[index][j] / imported_data[j][1]) * imported_data[j][3]
+    if U <= 1:
+        for (index, data) in enumerate(imported_data):
+            response_time_dict[index] = []
+            counter = 0
+            print("Tarefa ", index+1, "\n")
+            if not index:
+                response_time_dict[index] = [data[3]]
+                continue
+            j=0
+            while True :
+                # j = 0
+                if not counter:
+                    print("Primeira iteração")
+                    print("sum: ", data[3])
+                    response_time_dict[index] = [data[3]]            
+
+                else:
+                    print(counter+1, " iteração")
+                    
+                    sum = 0
                 
-                r = data[3] + sum
-                response_time_dict[index].extend(r)
+                    print("Percorrendo o conjunto")
+                    print("j: ", j, "\ncounter:", counter)
+                    
+                    for i in range (index):
+                        sum+= ceil(response_time_dict[index][j] / imported_data[i][1]) * imported_data[i][3]
 
-                if r == response_time_dict[index][-2]:
-                    break
+                    
+                    print("sum: ", sum)
+                    j+=1
+                    r = data[3] + sum
+                    print("Valor adicionado no array: ", r)
+                    response_time_dict[index].append(r)
+                    
+                # for j in range(index):
+                #     print("Percorrendo o conjunto")
+                #     print("j: ", j, "\ncounter:", counter)
+                #     print(response_time_dict[index])
+                #     print(response_time_dict[index][j])
+                #     sum += ceil(response_time_dict[index][j] / imported_data[j][1]) * imported_data[j][3]
+                #     j+=1
+                #     r = data[3] + sum
+                #     print("Valor adicionado no array: ", r)
+                #     response_time_dict[index].append(r)
+                
+                # r = data[3] + sum
+                # print("Valor adicionado no array: ", r)
+                # response_time_dict[index].append(r)
 
-                if r > data[2]:
-                    return data[0] + " não escalonável"
+                    if r == response_time_dict[index][-2]:
+                        print("Tarefa ", index+1, " ok")
+                        break
 
-            counter += 1
-    print(response_time_dict)    
+                    if r > data[2]:
+                        return {
+                            "data": response_time_dict, "status": "fail", "msg": "Atividade " + index+1 + "não é escalonável."
+                        }  
+                        # return data[0] + " não escalonável"
+
+                counter += 1
+            print("passou do while")
+        print(response_time_dict) 
+        
+        return {
+            "table": imported_data, "data": response_time_dict.values(), "status": "ok"
+        }    
+    else:
+        return {
+            "data": {}, "status": "fail", "msg": "Atividade " + index+1 + "não é escalonável."
+        }  
 
--- response_time/views.py
+++ response_time/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.views.generic import View, TemplateView, CreateView
 
 from .utils import analysis
@@ -9,12 +9,22 @@ from .utils import analysis
 def IndexView(request):
     base_template_name = 'base.html'
 
+    def get_context_data(self, *args, **kwargs):
+        # texts = ['Lorem ipsum', 'dolor sit amet', 'consectetur']
+        context = {
+        }
+        return context
+
     if request.method == "POST":
         result = analysis(request.FILES['myfile'])
+        
+        return render(request, 'index.html', {'context': result, 'base_template_name': base_template_name})
         
         	
 
     return render(request, 'index.html', {'base_template_name': base_template_name})
+
+
 
 
 index = IndexView
