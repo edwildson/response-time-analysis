06738f850d3dc1251a6651cb7f56b13561440139
Edwildson C. Rodrigues <edwildson.coelho@sysvale.com>
2022-06-14 09:03:16 -0300

    add jitter case

--- response_time/utils.py
+++ response_time/utils.py
@@ -7,6 +7,7 @@ def analysis(data):
     data_to_analysis = data
     dataset = Dataset()
     imported_data = dataset.load(data_to_analysis.read(),format='xlsx')
+    sucess = True
     for (index, data) in enumerate(imported_data):
         print(index, data)
     
@@ -15,81 +16,166 @@ def analysis(data):
 
     U = 0
 
-    for data in imported_data:
-        if None not in data:
+    # print('Coluna J: ' ,imported_data['J'])
+    # Checa se possue Jitter
+
+    jitter = False
+    for j in imported_data['J']:
+        if j is not None and j > 0:
+            jitter = True
+        
+
+    for (index, data) in enumerate(imported_data):
+        if data[3] is not None and data[1] is not None:
             U += data[3]/data[1]
         else:
-            del imported_data[-1]
+            print("wtf")
+            U = -1
+            continue
+            # del imported_data[index]
             
 
-    if U <= 1:
-        for (index, data) in enumerate(imported_data):
-            response_time_dict[index] = []
-            counter = 0
-            print("Tarefa ", index+1, "\n")
-            if not index:
-                response_time_dict[index] = [data[3]]
-                continue
-            j=0
-            while True :
-                # j = 0
-                if not counter:
-                    print("Primeira iteração")
-                    print("sum: ", data[3])
-                    response_time_dict[index] = [data[3]]            
+    if U <= 1 and U > 0:
+        print("opa")
+        if jitter is False:
+            for (index, data) in enumerate(imported_data):
+                response_time_dict[index] = []
+                counter = 0
+                print("Tarefa ", index+1, "\n")
+                if not index:
+                    response_time_dict[index] = [data[3]]
+                    continue
+                j=0
+                while True :
+                    # j = 0
+                    if not counter:
+                        print("Primeira iteração")
+                        print("sum: ", data[3])
+                        response_time_dict[index] = [data[3]]            
 
-                else:
-                    print(counter+1, " iteração")
+                    else:
+                        print(counter+1, " iteração")
+                        
+                        sum = 0
                     
-                    sum = 0
+                        print("Percorrendo o conjunto")
+                        print("j: ", j, "\ncounter:", counter)
+                        
+                        for i in range (index):
+                            sum+= ceil(response_time_dict[index][j] / imported_data[i][1]) * imported_data[i][3]
+
+                        
+                        print("sum: ", sum)
+                        j+=1
+                        r = data[3] + sum
+                        print("Valor adicionado no array: ", r)
+                        response_time_dict[index].append(r)
+                        
+                    # for j in range(index):
+                    #     print("Percorrendo o conjunto")
+                    #     print("j: ", j, "\ncounter:", counter)
+                    #     print(response_time_dict[index])
+                    #     print(response_time_dict[index][j])
+                    #     sum += ceil(response_time_dict[index][j] / imported_data[j][1]) * imported_data[j][3]
+                    #     j+=1
+                    #     r = data[3] + sum
+                    #     print("Valor adicionado no array: ", r)
+                    #     response_time_dict[index].append(r)
+                    
+                    # r = data[3] + sum
+                    # print("Valor adicionado no array: ", r)
+                    # response_time_dict[index].append(r)
+
+                        if r > data[2]:
+                            return {
+                                "table": imported_data, "data": response_time_dict.values(), "status": messages.ERROR, "message": "Tarefa não é escalonável."
+                            }  
+                            # return data[0] + " não escalonável"
+                        if r == response_time_dict[index][-2]:
+                            print("Tarefa ", index+1, " ok")
+                            break
+
+
+                    counter += 1
+                print("passou do while")
+                print(response_time_dict) 
                 
-                    print("Percorrendo o conjunto")
-                    print("j: ", j, "\ncounter:", counter)
-                    
-                    for i in range (index):
-                        sum+= ceil(response_time_dict[index][j] / imported_data[i][1]) * imported_data[i][3]
+            return {
+                "table": imported_data, "data": response_time_dict.values(), "message": "Tarefas escalonáveis", "status": messages.SUCCESS
+            }  
+        else:
+            for (index, data) in enumerate(imported_data):
+                response_time_dict[index] = []
+                counter = 0
+                print("Tarefa ", index+1, "\n")
+                if not index:
+                    response_time_dict[index] = [data[3]]
+                    continue
+                j=0
+                while True :
+                    # j = 0
+                    if not counter:
+                        print("Primeira iteração")
+                        print("sum: ", data[3])
+                        response_time_dict[index] = [data[3]]            
 
+                    else:
+                        print(counter+1, " iteração")
+                        
+                        sum = 0
                     
-                    print("sum: ", sum)
-                    j+=1
-                    r = data[3] + sum
-                    print("Valor adicionado no array: ", r)
-                    response_time_dict[index].append(r)
+                        print("Percorrendo o conjunto")
+                        print("j: ", j, "\ncounter:", counter)
+                        
+                        for i in range (index):
+                            sum+= ceil((response_time_dict[index][j]+imported_data[i][4]) / imported_data[i][1]) * imported_data[i][3]
+
+                        
+                        print("sum: ", sum)
+                        j+=1
+                        r = data[3] + sum
+                        print("Valor adicionado no array: ", r)
+                        response_time_dict[index].append(r)
+                        
+                    # for j in range(index):
+                    #     print("Percorrendo o conjunto")
+                    #     print("j: ", j, "\ncounter:", counter)
+                    #     print(response_time_dict[index])
+                    #     print(response_time_dict[index][j])
+                    #     sum += ceil(response_time_dict[index][j] / imported_data[j][1]) * imported_data[j][3]
+                    #     j+=1
+                    #     r = data[3] + sum
+                    #     print("Valor adicionado no array: ", r)
+                    #     response_time_dict[index].append(r)
                     
-                # for j in range(index):
-                #     print("Percorrendo o conjunto")
-                #     print("j: ", j, "\ncounter:", counter)
-                #     print(response_time_dict[index])
-                #     print(response_time_dict[index][j])
-                #     sum += ceil(response_time_dict[index][j] / imported_data[j][1]) * imported_data[j][3]
-                #     j+=1
-                #     r = data[3] + sum
-                #     print("Valor adicionado no array: ", r)
-                #     response_time_dict[index].append(r)
+                    # r = data[3] + sum
+                    # print("Valor adicionado no array: ", r)
+                    # response_time_dict[index].append(r)
+
+                        # if r+data[4] > data[2]:
+                        #     response_time_dict[index].append(r+data[4])
+                        #     return {
+                        #         "table": imported_data, "data": response_time_dict.values(), "status": messages.ERROR, "message": "Tarefa não é escalonável."
+                        #     }  
+                        #     # return data[0] + " não escalonável"
+                        if r == response_time_dict[index][-2]:
+                            
+                            response_time_dict[index].append(r+data[4])
+                            if r+data[4] > data[2]:
+                                sucess = False
+                            break
+
+
+                    counter += 1
+                print("passou do while")
+                print(response_time_dict) 
                 
-                # r = data[3] + sum
-                # print("Valor adicionado no array: ", r)
-                # response_time_dict[index].append(r)
+            return {
+                "table": imported_data, "data": response_time_dict.values(), "message": "Tarefas escalonáveis" if sucess else "Tarefas não escalonáveis", "status": messages.SUCCESS if sucess else messages.ERROR
+            }  
 
-                    if r > data[2]:
-                        return {
-                            "table": imported_data, "data": response_time_dict.values(), "status": messages.ERROR, "message": "Tarefa não é escalonável."
-                        }  
-                        # return data[0] + " não escalonável"
-                    if r == response_time_dict[index][-2]:
-                        print("Tarefa ", index+1, " ok")
-                        break
-
-
-                counter += 1
-            print("passou do while")
-        print(response_time_dict) 
-        
-        return {
-            "table": imported_data, "data": response_time_dict.values(), "message": "Tarefas escalonáveis", "status": messages.SUCCESS
-        }    
     else:
         return {
-           "table": imported_data, "data": {}, "status": messages.ERROR, "message": "Tarefas não escalonáveis, pois U>=1."
+           "table": imported_data, "data": {}, "status": messages.ERROR, "message": "Tarefas não escalonáveis, pois U>1."
         }  
 
